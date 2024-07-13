ac1f081ee4ffa17e5c4038bdaaaa3799ce0f02ee
ApMatheus <matheusaparecido101@gmail.com>
2024-07-13 09:58:24 -0300

    feat: carrousel brand

--- sections/Images/CarrouselBrand.tsx
+++ sections/Images/CarrouselBrand.tsx
@@ -15,18 +15,18 @@ export default function CarrouselBrand(props: Props) {
   const { imgs } = props;
 
   return (
-    <div class={"w-full flex gap-8 justify-between"}>
-      <ul class="w-auto h-full">
-        {imgs.map((item) => {
+    <div class={"w-full h-full flex   "}>
+      <ul class="w-full h-full flex flex-row gap-8 justify-between container py-8 divide-solid overflow-auto px-4 lg:px-0">
+        {imgs?.map((item) => (
           <Image
             src={item.src}
             alt={item.alt}
-            height={80}
+            height={50}
             width={200}
             fetchPriority="low"
             loading={"lazy"}
-          />;
-        })}
+          />
+        ))}
       </ul>
     </div>
   );
